7c3edf2af3b24a08468d6b2ea15a04d492bbdbe0
Bodhankar saketh <saketh954@gmail.com>
2021-09-20 13:45:17 +0530

    day2

---
+++ q1.js
@@ -0,0 +1,21 @@
+const countries = [
+    'ALBANIA',
+    'BOLIVIA',
+    'CANADA',
+    'DENMARK',
+    'ETHIOPIA',
+    'FINLAND',
+    'GERMANY',
+    'HUNGARY',
+    'IRELAND',
+    'JAPAN',
+    'KENYA'
+  ]
+  
+  const createArrayOfArrays = arr =>
+    arr.map(country => {
+      let name = country[0] + country.slice(1)
+      return [name, country.slice(0, 3), country.length]
+    })
+  
+  console.log(createArrayOfArrays(countries))
